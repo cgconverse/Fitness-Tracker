b0d658f7a9fb6227db397a7ff336fea0e0476a6a
CGC <70499508+cgconverse@users.noreply.github.com>
2021-01-10 14:52:56 -0800

    completed api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -21,26 +21,57 @@ router.get("/exercise", (req, res) => {
 // })
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
-    .then(workouts => {
-        res.json(workouts)
-    })
-} )
+        .then(workouts => {
+            res.json(workouts)
+        })
+})
 
 router.post("/api/workouts", (req, res) => {
     Workout.create({})
-    .then(workouts => {
-        res.json(workouts)
-    }) 
-    .catch(err => {
-        res.json(err)
-    })
-        
-} )
+        .then(workouts => {
+            res.json(workouts)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+
+})
 
 router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"))
 })
 
+router.get("/api/workouts/range", (req, res) => {
+    // aggregating code https://masteringjs.io/tutorials/mongoose/aggregate
+    Workout.aggregate([
+        {
+// aggregating the fields based off of total duration which is equal to the sum of the exercise duration
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" }
+            }
+        }
+    ])
+    // sorting the workout results by id and then only returning 7 results (7 days)
+        .sort({ _id: 1 })
+        .limit(7)
+        .then(results => {
+            res.json(results)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+})
+
+
+router.put( "/api/workouts/:id", ({body, params}, res) => {
+    Workout.findByIdAndUpdate(params.id, {exercises: body}, function(err, result) {
+        if(err) {
+            res.send(err)
+        } else {
+            res.send(result)
+        }
+    })
+})
 
 
 module.exports = router
